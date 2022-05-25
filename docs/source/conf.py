ffb6715dcf0f488d270020870d4634e758030e60
bobo <zhanghaibo8@foxmail.com>
2022-05-25 13:33:52 +0800

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'DCN-DOC'
+copyright = '2021, Huarong'
+author = 'Huarong'
 
 release = '0.1'
 version = '0.1.0'
